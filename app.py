d20fc2176d59ac4e3c50d924f030a7e43b912d56
Yihan Lin <62642577+lllllyh01@users.noreply.github.com>
2022-07-22 13:36:43 +0800

    Update app.py
    Old app.py suits local run, this commit edits it to suit Heroku.

--- app.py
+++ app.py
@@ -1,32 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-# from skimage import io
-from tensorflow.keras.models import load_model
-import cv2
+from keras.models import load_model
 from PIL import Image #use PIL
 import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def init():
     if request.method == 'POST':
         file = request.files['file']
-        # print("File Received")
+        print("File Received")
         filename = secure_filename(file.filename)
-        # print(filename)
-        file.save("./static/"+filename) #Heroku no need static
-        file = open("./static/"+filename,"r") #Heroku no need static
-        model = load_model("pneumonia")
-        image = cv2.imread("./static/"+filename)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        img = cv2.merge([gray,gray,gray])
+        print(filename)
+        # Open the image form working directory
+        image = Image.open(file)
+        model = load_model("Pneumonia")
+        img = np.asarray(image)
         img.resize((150,150,3))
         img = np.asarray(img, dtype="float32") #need to transfer to np to reshape
         img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2]) #rgb to reshape to 1,100,100,3
@@ -34,10 +24,5 @@ def upload_file():
         return(render_template("index.html", result=str(pred)))
     else:
         return(render_template("index.html", result="WAITING"))
-
-
-# In[2]:
-
-
 if __name__ == "__main__":
     app.run()
